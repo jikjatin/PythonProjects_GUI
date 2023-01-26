7d394aa54afc793223881f0da407f18c25ef6d91
jikjatin <jatinrajput04@gmail.com>
2023-01-26 23:52:16 +0530

    WIP

--- Indian States Quiz/main.py
+++ Indian States Quiz/main.py
@@ -7,7 +7,7 @@ image="India_map.gif"
 screen.addshape(image)
 turtle.shape(image)
 indian_states=screen.textinput(title="Guess the State",prompt="Enter name of the State").title()
-df=pandas.DataFrame("states.csv")
-print(df)
+data=pandas.read_csv("states.csv")
+print(data)
 screen.exitonclick()
 
