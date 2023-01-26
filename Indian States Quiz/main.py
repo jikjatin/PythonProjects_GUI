fae3de8827322336497fcd79c1ace8988c466969
jikjatin <jatinrajput04@gmail.com>
2023-01-27 00:26:55 +0530

    WIP

--- Indian States Quiz/main.py
+++ Indian States Quiz/main.py
@@ -1,13 +1,20 @@
 import turtle
 import pandas
 screen= turtle.Screen()
-
+my_tur=turtle.Turtle()
 screen.title("Indian States Game")
 image="India_map.gif"
 screen.addshape(image)
-turtle.shape(image)
-indian_states=screen.textinput(title="Guess the State",prompt="Enter name of the State").title()
+my_tur.shape(image)
+
 data=pandas.read_csv("states.csv")
-print(data)
+for state in data['state']:
+  indian_states=str(screen.textinput(title="Guess the State",prompt="Enter name of the State")).title()    
+  if indian_states==state:
+    my_tur.goto(int(data['x']),int(data['y']))
+    my_tur.write(f"{state}")
+    
+
+
 screen.exitonclick()
 
---
+++ Indian States Quiz/test.py
@@ -0,0 +1,6 @@
+import turtle
+my_tur=turtle.Turtle()    
+my_tur.goto(50,60)
+my_tur.write("state")
+screen=turtle.Screen()
+screen.exitonclick()
